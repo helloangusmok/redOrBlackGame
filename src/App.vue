3488f1fe771e1411ae307c5bfca0a41c97964b9d
helloangusmok <34521800+helloangusmok@users.noreply.github.com>
2018-03-01 20:02:50 +1100

    Shuffled deck function added

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-initial-deck></app-initial-deck>
+</template>
+
+<script>
+  import InitialDeck from './components/InitialDeck.vue'
+export default {
+  components: {
+    'app-initial-deck': InitialDeck
+  }
+}
+</script>
+
+<style>
+</style>
